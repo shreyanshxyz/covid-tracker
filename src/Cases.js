44b24bb1344af1c94227bd559cae5ab4eeb8f49c
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-01 21:00:04 +0530

    Added 4 types of divs(cards)

---
+++ src/Cases.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Cases.css";
+
+function Cases() {
+  return (
+    <div>
+      <div>Confirmed</div>
+      <div>Active</div>
+      <div>Recovered</div>
+      <div>Deceased</div>
+    </div>
+  );
+}
+
+export default Cases;
