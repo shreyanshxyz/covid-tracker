6e85eb196ec56b91e15785ac330f3307c417bacb
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-01 21:49:08 +0530

    Added Confirmed, Active, Deceased Cases

--- src/Cases.js
+++ src/Cases.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./Cases.css";
+import Confirmed from "./Confirmed";
+import Deceased from "./Deceased";
+import Recovered from "./Recovered";
 
 function Cases() {
   return (
-    <div>
-      <div>Confirmed</div>
+    <div className="cases__main">
+      <div>
+        <Confirmed />
+      </div>
       <div>Active</div>
-      <div>Recovered</div>
-      <div>Deceased</div>
+      <div>
+        <Recovered />
+      </div>
+      <div>
+        <Deceased />
+      </div>
     </div>
   );
 }
