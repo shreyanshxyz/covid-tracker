2d8ead6ef1758fd39f3fb1b52f6f6b164bd3bf6e
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-01 20:33:24 +0530

    Completed Outline of NavBar

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./Nav.css";
 
 function Nav() {
   return (
-    <div>
-      <h1>Navigation Bar</h1>
+    <div className="nav">
+      <div className="nav__left">
+        <h2>Logo</h2>
+      </div>
+
+      <div className="nav__right">
+        <h2>About</h2>
+        <h2>Resources</h2>
+        <h2>Dark</h2>
+      </div>
     </div>
   );
 }
