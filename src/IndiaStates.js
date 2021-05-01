1b3e69b484da2e070f581b6ed297ae19cafbb177
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-02 02:51:27 +0530

    Added Map with the API call for easier states names storage

--- src/IndiaStates.js
+++ src/IndiaStates.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
 
 function IndiaStates() {
+  const apiCall = "https://api.covid19india.org/data.json";
+  const [States, setStates] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(apiCall)
+      .then((res) => {
+        console.log(res.data.statewise);
+        setStates(res.data.statewise);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <FormControl className="state__dropdown">
+        <Select>
+          {States.map((s) => (
+            <MenuItem key={s.statecode}>{s.state}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </div>
   );
 }
