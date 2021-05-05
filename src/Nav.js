52832de9116131fe22cc3e09ba986951373e8762
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-05 20:49:29 +0530

    Logo (1)

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Nav.css";
+import Logo from "./images/cTracker.png";
 
 function Nav() {
   return (
     <div className="nav">
       <div className="nav__left">
-        <h2>Logo</h2>
+        <img className="nav__logo" src={Logo} alt="logo"></img>
       </div>
 
       <div className="nav__right">
