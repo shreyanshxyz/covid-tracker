90a33aaf35040483824766c959f0d92bc93cd4c3
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-05 21:23:45 +0530

    Added Graph secion. Work begins

--- src/Cases.js
+++ src/Cases.js
@@ -8,18 +8,24 @@ import Recovered from "./Recovered";
 
 function Cases() {
   return (
-    <div className="cases__main">
-      <div className="confirmed__cases">
-        <Confirmed />
+    <div className="case__graph">
+      <div className="cases__main">
+        <div className="confirmed__cases">
+          <Confirmed />
+        </div>
+        <div className="active__cases">
+          <Active />
+        </div>
+        <div className="recovered__cases">
+          <Recovered />
+        </div>
+        <div className="deceased__cases">
+          <Deceased />
+        </div>
       </div>
-      <div className="active__cases">
-        <Active />
-      </div>
-      <div className="recovered__cases">
-        <Recovered />
-      </div>
-      <div className="deceased__cases">
-        <Deceased />
+
+      <div className="cases__graph">
+        <h1>Graph</h1>
       </div>
     </div>
   );
