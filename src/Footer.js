aa600f9c4208e02666974cd561fdd895c0082594
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-08 20:18:52 +0530

    Completed the footer links added

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Footer.css";
+import { FiGithub, FiLinkedin, FiTwitter } from "react-icons/fi";
 
 function Footer() {
   return (
     <div className="footer__container">
       <p className="made__by"> &copy; Shreyansh Bhadoria | 2021 </p>
       <div className="social__media">
-        <p>kk</p>
-        <p>kk</p>
-        <p>kk</p>
+        <a href="https://github.com/shreyanshxyz/">
+          <FiGithub className="footer__icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/shreyanshxyz/">
+          <FiLinkedin className="footer__icon" />
+        </a>
+        <a href="https://twitter.com/ayyshreyy">
+          <FiTwitter className="footer__icon" />
+        </a>
       </div>
     </div>
   );
