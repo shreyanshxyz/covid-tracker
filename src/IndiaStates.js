95e407c32d6f2c13f61bb553736ac6d2cd863056
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-08 17:12:35 +0530

    Process Begun for Bar Graph addition

--- src/IndiaStates.js
+++ src/IndiaStates.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./IndiaStates.css";
-// import { Card } from "@material-ui/core";
 
 function IndiaStates() {
   const apiCall = "https://api.covid19india.org/data.json";
@@ -23,18 +22,6 @@ function IndiaStates() {
 
   // LAST UPDATED
   const [LastUpdated, setLastUpdated] = useState();
-
-  // useEffect(() => {
-  //   axios
-  //     .get(apiCall)
-  //     .then((res) => {
-  //       console.log(res.data.statewise);
-  //       setStates(res.data.statewise);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
 
   const stateAssign = (e) => {
     axios.get(apiCall).then((res) => {
@@ -61,81 +48,83 @@ function IndiaStates() {
   };
 
   return (
-    <div className="drop__cards">
-      <div className="dropdown__div">
-        <select className="drop__original" onChange={stateAssign}>
-          <option value="none" selected disabled hidden>
-            Select A State
-          </option>
-          <option value={0}>Total</option>
-          <option value={1}>Maharashtra</option>
-          <option value={2}>Kerala</option>
-          <option value={3}>Karnataka</option>
-          <option value={4}>Andhra Pradesh</option>
-          <option value={5}>Tamil Nadu</option>
-          <option value={6}>Delhi</option>
-          <option value={7}>Uttar Pradesh</option>
-          <option value={8}>West Bengal</option>
-          <option value={9}>Odisha</option>
-          <option value={10}>Rajasthan</option>
-          <option value={11}>Chhattisgarh</option>
-          <option value={12}>Telangana</option>
-          <option value={13}>Haryana</option>
-          <option value={14}>Gujarat</option>
-          <option value={15}>Bihar</option>
-          <option value={16}>Madhya Pradesh</option>
-          <option value={17}>Assam</option>
-          <option value={18}>Punjab</option>
-          <option value={19}>Jammu and Kashmir</option>
-          <option value={20}>Jharkhand</option>
-          <option value={21}>Uttarakhand</option>
-          <option value={22}>Himachal Pradesh</option>
-          <option value={23}>Goa</option>
-          <option value={24}>Puducherry</option>
-          <option value={25}>Tripura</option>
-          <option value={26}>Manipur</option>
-          <option value={27}>Chandigarh</option>
-          <option value={28}>Arunachal Pradesh</option>
-          <option value={29}>Meghalaya</option>
-          <option value={30}>Nagaland</option>
-          <option value={31}>Ladakh</option>
-          <option value={32}>Sikkim</option>
-          <option value={33}>Andaman and Nicobar Islands</option>
-          <option value={34}>Mizoram</option>
-          <option value={35}>Dadra and Nagar Haveli and Daman and Diu</option>
-          <option value={36}>Lakshadweep</option>
-        </select>
-      </div>
-
-      <div className="casesdropdown__cards">
-        <div className="row1">
-          <div className="confirmed__card">
-            <p>Confirmed</p>
-            <h4>+ {DailyConfirmed}</h4>
-            <h3>{Confirmed}</h3>
-          </div>
-
-          <div className="active__card">
-            <p>Active</p>
-            <h3>{Active}</h3>
-          </div>
+    <div className="card__graph">
+      <div className="drop__cards">
+        <div className="dropdown__div">
+          <select className="drop__original" onChange={stateAssign}>
+            <option value="none" selected disabled hidden>
+              Select A State
+            </option>
+            <option value={0}>Total</option>
+            <option value={1}>Maharashtra</option>
+            <option value={2}>Kerala</option>
+            <option value={3}>Karnataka</option>
+            <option value={4}>Andhra Pradesh</option>
+            <option value={5}>Tamil Nadu</option>
+            <option value={6}>Delhi</option>
+            <option value={7}>Uttar Pradesh</option>
+            <option value={8}>West Bengal</option>
+            <option value={9}>Odisha</option>
+            <option value={10}>Rajasthan</option>
+            <option value={11}>Chhattisgarh</option>
+            <option value={12}>Telangana</option>
+            <option value={13}>Haryana</option>
+            <option value={14}>Gujarat</option>
+            <option value={15}>Bihar</option>
+            <option value={16}>Madhya Pradesh</option>
+            <option value={17}>Assam</option>
+            <option value={18}>Punjab</option>
+            <option value={19}>Jammu and Kashmir</option>
+            <option value={20}>Jharkhand</option>
+            <option value={21}>Uttarakhand</option>
+            <option value={22}>Himachal Pradesh</option>
+            <option value={23}>Goa</option>
+            <option value={24}>Puducherry</option>
+            <option value={25}>Tripura</option>
+            <option value={26}>Manipur</option>
+            <option value={27}>Chandigarh</option>
+            <option value={28}>Arunachal Pradesh</option>
+            <option value={29}>Meghalaya</option>
+            <option value={30}>Nagaland</option>
+            <option value={31}>Ladakh</option>
+            <option value={32}>Sikkim</option>
+            <option value={33}>Andaman and Nicobar Islands</option>
+            <option value={34}>Mizoram</option>
+            <option value={35}>Dadra and Nagar Haveli and Daman and Diu</option>
+            <option value={36}>Lakshadweep</option>
+          </select>
         </div>
 
-        <div className="row2">
-          <div className="recovered__card">
-            <p>Recovered</p>
-            <h4>+ {DailyRecovered}</h4>
-            <h3>{Recovered}</h3>
+        <div className="casesdropdown__cards">
+          <div className="row1">
+            <div className="confirmed__card">
+              <p>Confirmed</p>
+              <h4>+ {DailyConfirmed}</h4>
+              <h3>{Confirmed}</h3>
+            </div>
+
+            <div className="active__card">
+              <p>Active</p>
+              <h3>{Active}</h3>
+            </div>
           </div>
 
-          <div className="deaths__card">
-            <p>Deaths</p>
-            <h4>+ {DailyDeaths}</h4>
-            <h3>{Deaths}</h3>
+          <div className="row2">
+            <div className="recovered__card">
+              <p>Recovered</p>
+              <h4>+ {DailyRecovered}</h4>
+              <h3>{Recovered}</h3>
+            </div>
+
+            <div className="deaths__card">
+              <p>Deaths</p>
+              <h4>+ {DailyDeaths}</h4>
+              <h3>{Deaths}</h3>
+            </div>
           </div>
-        </div>
-        <div className="row3">
-          <h4 className="last__update">Last Updated: {LastUpdated}</h4>
+          <div className="row3">
+            <h4 className="last__update">Last Updated: {LastUpdated}</h4>
+          </div>
         </div>
       </div>
     </div>
