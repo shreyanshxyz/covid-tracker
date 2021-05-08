a6b8067fda90ef960051b9e475cb007c62185657
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-08 19:44:34 +0530

    Small Changes

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div>
-      <h3>Footer</h3>
+    <div className="footer__container">
+      <h1 className="made__by">Salutation</h1>
+      <div className="social__media">Jah</div>
     </div>
   );
 }
