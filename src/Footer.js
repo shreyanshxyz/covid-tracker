c2ac0541fbc1773b0cdf89fa9e1342fc799b566d
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-08 20:21:09 +0530

    Links now open in new tabs

--- src/Footer.js
+++ src/Footer.js
@@ -7,13 +7,13 @@ function Footer() {
     <div className="footer__container">
       <p className="made__by"> &copy; Shreyansh Bhadoria | 2021 </p>
       <div className="social__media">
-        <a href="https://github.com/shreyanshxyz/">
+        <a href="https://github.com/shreyanshxyz/" target="_blank">
           <FiGithub className="footer__icon" />
         </a>
-        <a href="https://www.linkedin.com/in/shreyanshxyz/">
+        <a href="https://www.linkedin.com/in/shreyanshxyz/" target="_blank">
           <FiLinkedin className="footer__icon" />
         </a>
-        <a href="https://twitter.com/ayyshreyy">
+        <a href="https://twitter.com/ayyshreyy" target="_blank">
           <FiTwitter className="footer__icon" />
         </a>
       </div>
